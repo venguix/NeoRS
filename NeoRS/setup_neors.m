5c9c9ebb811557e805cc6b8e17acfa7cb3860596
Vicente Enguix <venguix@users.noreply.github.com>
2021-10-29 10:44:19 -0400

    solved neors_path problem

--- NeoRS/setup_neors.m
+++ NeoRS/setup_neors.m
@@ -3,6 +3,7 @@ function [atlas1mm,atlas3mm, ROI ] = setup_neors()
 warning off
 
 neors_path=which('main_neors');
+neors_path=neors_path(1:end-13);
 
 % Load Atlas
 atlas=[neors_path '/atlases/atlases_st_louis/'];
