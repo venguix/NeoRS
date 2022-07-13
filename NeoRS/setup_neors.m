4b7aa89c6675c9fe005c2d2fd62973c7a2227328
Vicente Enguix <venguix@users.noreply.github.com>
2022-07-13 14:06:19 -0400

    corrected setup neors for new template

--- NeoRS/setup_neors.m
+++ NeoRS/setup_neors.m
@@ -6,20 +6,11 @@ neors_path=which('main_neors');
 neors_path=neors_path(1:end-13);
 
 % Load Atlas
-% atlas=[neors_path '/atlases/atlases_st_louis/'];
-% ROI=[neors_path '/atlases/ROI'];
-
 atlas=[neors_path '/atlases3/'];
 ROI=[neors_path '/atlases3/ROI'];
 
-%atlas1mm=[atlas 'term_N10_t2w_on_711-2N_111.nii.gz'];
-%atlas3mm=[atlas 'term_N10_t2w_on_711-2N_333.nii.gz'];
-
-%atlas1mm=[atlas 'dhcp_2_tailarach_1.nii.gz'];
-%atlas3mm=[atlas 'template_T2_40_2mm.nii'];
-
-atlas1mm=[atlas 'template_chusj.nii.gz'];
-atlas3mm=[atlas 'template_chusj_3mm.nii.gz'];
+atlas1mm=[atlas 'dhcp2tailarach_1.nii.gz'];
+atlas3mm=[atlas 'dhcp2tailarach_3.nii.gz'];
 
 
 
